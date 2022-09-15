b7289d5942edfb25acdcb48e2216c26168363065
Eetu Runonen <eeturunonen@edu93-170.turkuamk.fi>
2022-09-15 09:38:28 +0300

    Header päivitys

--- filu.py
+++ filu.py
@@ -1,7 +1,8 @@
 ###################################################################
-###     Sovellusohjelmoinnin kurssi                             ###
-###     Eetu Runonen                                            ###
-###     2022                                                    ###
+###     Sovellusohjelmoinnin kurssi S22                         ###
+###                                                             ###
+###     Eetu Runonen PTIVIS21E                                  ###
+###     eetu.runonen@edu.turkuamk.fi                            ###
 ###                                                             ###
 ###     e-Kirja kurssille: Python Crash Course, 2nd Edition:    ###
 ###     A Hands-On, Project-Based Introduction to Programming   ###
