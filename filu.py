27c95e2cc425352a8563710a7c6f7fca58eea0b9
Eetu Runonen <eeturunonen@edu93-170.turkuamk.fi>
2022-09-15 09:05:19 +0300

    First commit.

---
+++ filu.py
@@ -0,0 +1,8 @@
+###################################################################
+###     Sovellusohjelmoinnin kurssi                             ###
+###     Eetu Runonen                                            ###
+###     2022                                                    ###
+###                                                             ###
+###     e-Kirja kurssille: Python Crash Course, 2nd Edition:    ###
+###     A Hands-On, Project-Based Introduction to Programming   ###
+###################################################################
