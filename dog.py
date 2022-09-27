bb50bd0a9546bba9765e317cf5e538c5c507ab9f
Eetu Runonen <eeturunonen@edu93-158.turkuamk.fi>
2022-09-27 18:23:28 +0300

    testimonals.py added

--- dog.py
+++ dog.py
@@ -1,10 +1,11 @@
 class Dog:
     """A simple attempt to model a dog."""
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, color):
         """Initialize name and age attributes."""
         self.name = name
         self.age = age
+        self.color = color
 
     def sit(self):
         """Simulate a dog sitting in response to a command."""
@@ -13,3 +14,7 @@ class Dog:
     def roll_over(self):
         """Simulate rolling over in response to a command."""
         print(f"{self.name} rolled over!")
+
+    def bark(self):
+        """Simulate a dog barking"""
+        print(f"{self.name} *wuf* *wuf*")
---
+++ testimonals.py
@@ -0,0 +1,5 @@
+from dog.py import Dog
+
+dog1 = Dog("Karri", 2, "Red")
+
+dog1.bark1()
